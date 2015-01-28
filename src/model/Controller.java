7dbbd47ad7b8ca7dad0d061c79df4e31764a615e
johnyak <johnyak381@gmail.com>
2015-01-27 21:14:19 -0500

    Controller started

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,11 +1,17 @@
 package model;
 
 public class Controller {
+	ArrayList<Card> builtDeck = new ArrayList<Card>();
 	
+	public void buildDeck(){
+		
+	}
 	
 }
 
 /*
+ * 
+ * 
  * new deck phase
  * dealing players phase
  * player turn phase
